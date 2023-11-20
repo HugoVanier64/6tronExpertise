fb0e30a3d6efd653a61f3a47376f81e2c5d37a61
HugoVanier64 <hugo.vanier@etu.estia.fr>
2023-11-20 15:47:05 +0100

    timer

--- main.cpp
+++ main.cpp
@@ -3,15 +3,35 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
-
+using namespace std::chrono;
 namespace {
-#define PERIOD_MS 2000ms
+#define PERIOD_MS 1000ms
+InterruptIn boutonPin(BUTTON1);  // Numéro de la broche du bouton
+DigitalOut ledPin(LED1);   // Numéro de la broche de la LED 
+Timer t;
+}
+
+
+
+void flipAllumage()
+{
+    ledPin = 1;
+	t.start();
+}
+
+void flipEteindre()
+{
+    ledPin = 0;
+	t.stop();
 }
 
 int main()
 {
+	boutonPin.rise(&flipAllumage);
+	boutonPin.fall(&flipEteindre);
 	while (true) {
-		printf("Alive! \n");
-		ThisThread::sleep_for(PERIOD_MS / 2);
+		
+		 printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+		ThisThread::sleep_for(PERIOD_MS / 8);
 	}
 }
