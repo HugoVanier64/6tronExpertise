c67604cd11e2373cd2bf021c7b9f126ec7ccc628
HugoVanier64 <hugo.vanier@etu.estia.fr>
2023-11-21 13:22:59 +0100

    TempAvec6tron

--- main.cpp
+++ main.cpp
@@ -3,38 +3,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
+#include "bme280.h"
 using namespace std::chrono;
+using namespace sixtron;
 namespace {
 #define PERIOD_MS 1000ms
 #define BME280_adresse 0x76 << 1
-I2C bus(I2C1_SDA,I2C1_SCL);  // Numéro de la broche du bouton
+I2C bus(I2C1_SDA,I2C1_SCL); 
+BME280 CapteurBME280(&bus, BME280::I2CAddress::Address1);
 }
-
 
 int main()
 {
-	
-	
+	CapteurBME280.initialize();
+	CapteurBME280.set_sampling();
+
 	while (true) {
-		char tempRegMeasCtrl = 0xF4;
-    	char modeNormal = 0b00100111;
-
-    	bus.write(BME280_adresse, &tempRegMeasCtrl, 1);
-    	bus.write(BME280_adresse, &modeNormal, 1);
+		
+		
+		float temperature = CapteurBME280.temperature();
     	
-    	char tempRegAddr1 = 0xFA;
-    	char msb_lsb_xlsb[3];
-    	bus.write(BME280_adresse, &tempRegAddr1, 1);
-    	bus.read(BME280_adresse, msb_lsb_xlsb, 3);
-
-    	printf("MSB: %02X\n", msb_lsb_xlsb[0]);
-    	printf("LSB: %02X\n", msb_lsb_xlsb[1]);
-    	printf("XLSB: %02X\n", msb_lsb_xlsb[2]);
-
-
-    	int temp = (msb_lsb_xlsb[0] << 12 | msb_lsb_xlsb[1] << 4 | msb_lsb_xlsb[2]);
-
-    	printf("Temperature: %.2d °C \n", temp);
+        printf("Temperature: %.2f °C \n", temperature);
 		ThisThread::sleep_for(PERIOD_MS);
 	}
 }
