d5909a2a580c934a511563c76a90bebf8fd9eb09
HugoVanier64 <hugo.vanier@etu.estia.fr>
2023-11-21 16:23:00 +0100

    rtosTPpasfini

--- main.cpp
+++ main.cpp
@@ -5,49 +5,107 @@
 #include "mbed.h"
 #include "bme280.h"
 using namespace std::chrono;
+using namespace sixtron;
 namespace {
 #define PERIOD_MS 1000ms
 #define BME280_adresse 0x76 << 1
-I2C bus(I2C1_SDA,I2C1_SCL);  // Numéro de la broche du bouton
+I2C bus(I2C1_SDA,I2C1_SCL); 
+InterruptIn Bouton(BUTTON1);
+Thread thread;
+Thread thread2;
+Timer timer;
+Timer timer2;
+float pression = 0.0;
+float humidite = 0.0;
+float temperature = 0.0;
+bool appuie = false;
+Mutex mutex_temp;
+Mutex mutex_hum;
+Mutex mutex_press;
+Mutex mutex_print;
+
+BME280 CapteurBME280(&bus, BME280::I2CAddress::Address1);
 }
 
+void obtenirTemp(){
+
+	mutex_temp.lock();
+	float temperature = CapteurBME280.temperature();
+	mutex_temp.unlock();
+}
+
+void obtenirHum(){
+
+	mutex_temp.lock();
+	float humidite = CapteurBME280.humidity();
+	mutex_temp.unlock();
+}
+
+void threadTemp(){
+
+	CapteurBME280.initialize();
+	CapteurBME280.set_sampling();
+
+	while(true){
+
+		timer.start();
+		printf("%f", timer.elapsed_time().count());
+		if(timer.elapsed_time().count() == 2){
+			mutex_print.lock();
+			printf("Température: %.2f °C \n", temperature);	
+			mutex_print.unlock();
+			timer.reset();
+		}
+		ThisThread::sleep_for(PERIOD_MS);
+		
+	}
+}
+
+void threadHum(){
+
+	while(true){
+
+		timer2.start();
+		if(timer2.elapsed_time().count() == 2.0){
+			mutex_print.lock();
+			printf("Humidite: %.2f °C \n", humidite);	
+			mutex_print.unlock();
+			timer2.reset();
+		}
+		ThisThread::sleep_for(PERIOD_MS);
+		
+	}
+}
+
+void obtenirPression(){
+	
+	appuie = true;
+}
 
 int main()
 {
-		// 0xF5
-		char config[2] = {0xF5,0b10110100 };
-    	bus.write(BME280_adresse, config, 2);	
+	CapteurBME280.initialize();
+	CapteurBME280.set_sampling();
 
-		//0xF4
-		char ctrl_meas[2] = {0xF4,0b00100111 };
-		bus.write(BME280_adresse, ctrl_meas, 2);
-
+	//thread.start(threadTemp);
+	//thread2.start(threadHum);
 
 	while (true) {
-		
-		
-		//0xF3
-		char status = 0b11110011;
-		char update[1]; 
-		bus.write(BME280_adresse, &status, 1);
-		bus.read(BME280_adresse, update, 1);
-		
 
-    	char tempRegAddr1 = 0xFA;
-    	char msb_lsb_xlsb[3];
-    	bus.write(BME280_adresse, &tempRegAddr1, 1);
-    	bus.read(BME280_adresse, msb_lsb_xlsb, 3);
-
-    	printf("MSB: %X\n", msb_lsb_xlsb[0]);
-    	printf("LSB: %X\n", msb_lsb_xlsb[1]);
-    	printf("XLSB: %X\n", msb_lsb_xlsb[2]);
-		printf("update: %X\n", update[0]);
+		Bouton.rise(&obtenirPression);
 		
+		if(appuie){
+			mutex_press.lock();
+			float pression = CapteurBME280.pressure();
+			mutex_press.unlock();
+			mutex_print.lock();
+        	printf("Pressure: %.2f °C \n", pression);
+			mutex_print.unlock();
+			appuie = false;
+		}
 
-    	int temp = (msb_lsb_xlsb[0] << 12 | msb_lsb_xlsb[1] << 4 | msb_lsb_xlsb[2]);
-
-    	printf("Temperature: %.2d °C \n", temp);
+		//thread.join();
+		//thread2.join();
 		ThisThread::sleep_for(PERIOD_MS);
 	}
-
 }
