02a4058b27b71e82d8a45d76537674c28651c444
HugoVanier64 <hugo.vanier@etu.estia.fr>
2023-11-21 12:13:15 +0100

    main_areprendre

--- main.cpp
+++ main.cpp
@@ -6,46 +6,35 @@
 using namespace std::chrono;
 namespace {
 #define PERIOD_MS 1000ms
-Ticker flipper;
-InterruptIn boutonPin(BUTTON1);  // Numéro de la broche du bouton
-DigitalOut ledPin(LED1);   // Numéro de la broche de la LED 
-float freq = 5.0;
+#define BME280_adresse 0x76 << 1
+I2C bus(I2C1_SDA,I2C1_SCL);  // Numéro de la broche du bouton
 }
-
-void flip()
-{
-    ledPin = !ledPin;
-	
-}
-
-void flipBouton1()
-{
-    freq++;
-}
-
-void flipBouton2()
-{
-	if(freq >= 20.0){
-		freq = 5.0;
-		flipper.attach(&flip, freq); 
-	}
-	else{
-		flipper.attach(&flip, freq);
-	}
-	
-}
-
 
 
 int main()
 {
-	ledPin = 1;
-	boutonPin.rise(&flipBouton1);
-	boutonPin.fall(&flipBouton2);
-	flipper.attach(&flip, freq); 
+	
+	
 	while (true) {
-		
-		printf("The actual frequence is %f seconds\n", freq);
-		ThisThread::sleep_for(PERIOD_MS / 4);
+		char tempRegMeasCtrl = 0xF4;
+    	char modeNormal = 0b00100111;
+
+    	bus.write(BME280_adresse, &tempRegMeasCtrl, 1);
+    	bus.write(BME280_adresse, &modeNormal, 1);
+    	
+    	char tempRegAddr1 = 0xFA;
+    	char msb_lsb_xlsb[3];
+    	bus.write(BME280_adresse, &tempRegAddr1, 1);
+    	bus.read(BME280_adresse, msb_lsb_xlsb, 3);
+
+    	printf("MSB: %02X\n", msb_lsb_xlsb[0]);
+    	printf("LSB: %02X\n", msb_lsb_xlsb[1]);
+    	printf("XLSB: %02X\n", msb_lsb_xlsb[2]);
+
+
+    	int temp = (msb_lsb_xlsb[0] << 12 | msb_lsb_xlsb[1] << 4 | msb_lsb_xlsb[2]);
+
+    	printf("Temperature: %.2d °C \n", temp);
+		ThisThread::sleep_for(PERIOD_MS);
 	}
 }
